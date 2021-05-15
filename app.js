653125e612a89e93c50f0d81d042c29994a41c57
jose <jmolina721@gmail.com>
2021-05-15 13:55:58 -0500

    Added functionality to display and select featured/non-featured projects.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer
@@ -134,5 +136,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Page created! Check out index.html in this directory to see it!');
+    });
     });
